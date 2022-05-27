afb1b382d811d2ff29b57871c8e1faeec87e5c61
JackieZ1412 <JackieZ1412@github.com>
2022-05-27 11:37:02 +0800

    finished cache lab

--- CSAPP/cachelab-handout/trans.c
+++ CSAPP/cachelab-handout/trans.c
@@ -22,6 +22,118 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int a1,a2,a3,a4,a5,a6,a7,a8;
+    if(N == 32){
+        for(int i = 0;i < 4;i++){
+            for(int j = 0;j < 4;j++){
+                for(int k = (i << 3); k < ((i + 1) << 3);k++){
+                    int h = j << 3;
+                    a1 = A[k][h];
+                    a2 = A[k][h + 1];
+                    a3 = A[k][h + 2];
+                    a4 = A[k][h + 3];
+                    a5 = A[k][h + 4];
+                    a6 = A[k][h + 5];
+                    a7 = A[k][h + 6];
+                    a8 = A[k][h + 7];
+                    
+                    B[h][k] = a1;
+                    B[h + 1][k] = a2;
+                    B[h + 2][k] = a3;
+                    B[h + 3][k] = a4;
+                    B[h + 4][k] = a5;
+                    B[h + 5][k] = a6;
+                    B[h + 6][k] = a7;
+                    B[h + 7][k] = a8;
+                }
+            }
+        }
+    }
+    else if(N == 64){
+        for(int i = 0;i < 64;i += 8){
+            for(int j = 0;j < 64;j += 8){
+                for(int k = j;k < j + 4;k++){
+                    a1 = A[k][i];
+                    a2 = A[k][i + 1];
+                    a3 = A[k][i + 2];
+                    a4 = A[k][i + 3];
+                    a5 = A[k][i + 4];
+                    a6 = A[k][i + 5];
+                    a7 = A[k][i + 6];
+                    a8 = A[k][i + 7];
+
+                    B[i][k] = a1;
+                    B[i][k + 4] = a5;
+                    B[i + 1][k] = a2;
+                    B[i + 1][k + 4] = a6;
+                    B[i + 2][k] = a3;
+                    B[i + 2][k + 4] = a7;
+                    B[i + 3][k] = a4;
+                    B[i + 3][k + 4] = a8;
+                }
+                for(int k = i;k < i + 4;k++){
+                    a1 = B[k][j + 4];
+                    a2 = B[k][j + 5];
+                    a3 = B[k][j + 6];
+                    a4 = B[k][j + 7];
+                    a5 = A[j + 4][k];
+                    a6 = A[j + 5][k];
+                    a7 = A[j + 6][k];
+                    a8 = A[j + 7][k];
+
+                    B[k][j + 4] = a5;
+                    B[k][j + 5] = a6;
+                    B[k][j + 6] = a7;
+                    B[k][j + 7] = a8;
+                    B[k + 4][j] = a1;
+                    B[k + 4][j + 1] = a2;
+                    B[k + 4][j + 2] = a3;
+                    B[k + 4][j + 3] = a4;
+                }
+                for(int k = i + 4;k < i + 8;k++){
+                     a1 = A[j + 4][k];
+                     a2 = A[j + 5][k];
+                     a3 = A[j + 6][k];
+                     a4 = A[j + 7][k];
+
+                     B[k][j + 4] = a1;
+                     B[k][j + 5] = a2;
+                     B[k][j + 6] = a3;
+                     B[k][j + 7] = a4;
+                }
+            }
+        }
+    }
+    else{
+        for(int i = 0;i < N;i += 8){
+            for(int j = 0;j < M;j++){
+                if(i + 8 <= N && j < M){
+                    a1 = A[i][j];
+                    a2 = A[i + 1][j];
+                    a3 = A[i + 2][j];
+                    a4 = A[i + 3][j];
+                    a5 = A[i + 4][j];
+                    a6 = A[i + 5][j];
+                    a7 = A[i + 6][j];
+                    a8 = A[i + 7][j];
+                    
+                    B[j][i] = a1;
+                    B[j][i + 1] = a2;
+                    B[j][i + 2] = a3;
+                    B[j][i + 3] = a4;
+                    B[j][i + 4] = a5;
+                    B[j][i + 5] = a6;
+                    B[j][i + 6] = a7;
+                    B[j][i + 7] = a8;
+                }
+                else{
+                    for(int k = i; k < i + 8 && k < N; k++){
+                        B[j][k] = A[k][j];
+                    }
+                }
+            }
+        }
+    }
 }
 
 /* 
@@ -81,4 +193,5 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
     }
     return 1;
 }
+
 
